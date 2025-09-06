8d4d3aad8b91510c335c719e2c3e0e2d42a6fd3c
ahonafredowanhasib <4hon4f.redowan.hasib.03@gmail.com>
2025-09-06 10:59:18 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const photos = ["imgs/meme1.jpg", "imgs/meme2.jpg", "imgs/meme3.jpg"]
+const imgTag = document.querySelector('img')
+
+let count = 0
+function next(){
+    count++
+    if(count >= photos.length){
+        count = 0
+    }
+    imgTag.src = photos[count]
+}
+
+function prev(){
+    count--
+    if(count < 0){
+        count = photos.length - 1 
+    }
+    imgTag.src = photos[count]
+}
