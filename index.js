984b8a161d067978ca7be3e357232254cc80ed75
ahonafredowanhasib <4hon4f.redowan.hasib.03@gmail.com>
2025-09-06 12:09:44 +0600

    multiple button test

--- index.js
+++ index.js
@@ -37,3 +37,16 @@ rbutn.addEventListener('click', function removeStyle(){
     para.classList.remove("p-style")
 })
 
+
+
+// multiple button 
+
+    const len = document.querySelectorAll(".myButton").length;
+
+        for(let i = 0 ; i < len ; i++){
+            document.querySelectorAll(".myButton")[i].addEventListener("click", function(){
+
+            let text = this.innerHTML;
+            document.querySelector("h1").innerHTML = text + " is clicked";
+            })
+        }
